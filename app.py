2205451e1399c9e0d311a8b46b22b6208ee204bc
IvanMarinov9b15 <ipmarinov248b@gmail.com>
2026-01-15 12:51:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if "sports" not in st.session_state:
 st.subheader("Избери любими неща")
 
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.select box("Любим спорт:", list(st.session_state.sports.keys()))
+sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
 
 if st.button("Запази избора"):
     st.session_state.colors[color] += 1
