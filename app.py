f14273c3a71b5aec67a24e74fae9915a16bdd94f
IvanMarinov9b15 <ipmarinov248b@gmail.com>
2026-01-15 12:54:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Любими неща – класна анкета")
+st.title("Училищен дневник - Оценки")
 
-# Инициализация на данните
-if "colors" not in st.session_state:
-    st.session_state.colors = {
-        "Червен": 0,
-        "Син": 0,
-        "Зелен": 0,
-        "Жълт": 0
+# Инициализация на данните за оценки
+# По скалата от 2 до 6
+if "grades" not in st.session_state:
+    st.session_state.grades = {
+        "Отличен (6)": 0,
+        "Много добър (5)": 0,
+        "Добър (4)": 0,
+        "Среден (3)": 0,
+        "Слаб (2)": 0
     }
 
-if "sports" not in st.session_state:
-    st.session_state.sports = {
-        "Футбол": 0,
-        "Баскетбол": 0,
-        "Волейбол": 0,
-        "Плуване": 0
-    }
+# Списък с ученици (можеш да добавиш още имена тук)
+students_list = ["Иван Петров", "Мария Георгиева", "Георги Иванов", "Елена Стоянова"]
 
-st.subheader("Избери любими неща")
+st.subheader("Въвеждане на нова оценка")
 
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+# Избор на ученик и оценка
+student = st.selectbox("Избери ученик:", students_list)
+grade = st.selectbox("Избери оценка:", list(st.session_state.grades.keys()))
 
-if st.button("Запази избора"):
-    st.session_state.colors[color] += 1
-    st.session_state.sports[sport] += 1
-    st.success("Изборът е записан!")
+if st.button("Запази оценката"):
+    st.session_state.grades[grade] += 1
+    st.success(f"Оценката на {student} е записана успешно!")
 
 st.divider()
 
-st.subheader("Резултати")
+st.subheader("Статистика на класа")
 
-# Графика за цветовете
-st.write("Любими цветове")
-colors_df = pd.DataFrame.from_dict(
-    st.session_state.colors, orient="index", columns=["Брой"]
+# Графика за разпределението на оценките
+st.write("Разпределение на оценките по брой ученици")
+grades_df = pd.DataFrame.from_dict(
+    st.session_state.grades, orient="index", columns=["Брой ученици"]
 )
-st.bar_chart(colors_df)
 
-# Графика за спортовете
-st.write("Любими спортове")
-sports_df = pd.DataFrame.from_dict(
-    st.session_state.sports, orient="index", columns=["Брой"]
-)
-st.bar_chart(sports_df)
+# Показваме колонна диаграма
+st.bar_chart(grades_df)
+
+# Показваме и таблица за по-голяма точност
+st.table(grades_df)
